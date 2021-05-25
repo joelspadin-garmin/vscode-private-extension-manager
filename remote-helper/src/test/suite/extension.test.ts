db680aee1bef899f503b5c6d7df93482a8ed4563
Joel Spadin <joel.spadin@garmin.com>
2021-05-25 13:50:36 -0500

    Clean up remote-helper tests

--- remote-helper/src/test/suite/extension.test.ts
+++ remote-helper/src/test/suite/extension.test.ts
@@ -3,14 +3,18 @@ import { before } from 'mocha';
 import 'source-map-support/register';
 import * as vscode from 'vscode';
 
+const EXTENSION_NAME = 'private-extension-manager-remote-helper';
+const PUBLISHER_ID = 'garmin';
+const EXTENSION_ID = `${PUBLISHER_ID}.${EXTENSION_NAME}`;
+
 suite('Extension Test Suite', function () {
     vscode.window.showInformationMessage('Start all tests.');
 
     before(function () {
-        const extension = vscode.extensions.getExtension('garmin.private-extension-manager-remote-helper');
+        const extension = vscode.extensions.getExtension(EXTENSION_ID);
 
         if (!extension) {
-            throw Error('garmin.private-extension-manager-remote-helper must be installed.');
+            throw Error(`${EXTENSION_ID} must be installed.`);
         }
 
         if (extension.extensionKind !== vscode.ExtensionKind.UI) {
@@ -21,14 +25,14 @@ suite('Extension Test Suite', function () {
     test('Get Package JSON', async function () {
         const extension = await vscode.commands.executeCommand<Partial<vscode.Extension<any>>>(
             '_privateExtensionManager.remoteHelper.getExtension',
-            'garmin.private-extension-manager-remote-helper',
+            EXTENSION_ID,
         );
 
         assert.notStrictEqual(extension, undefined);
-        assert.strictEqual(extension?.id?.toLowerCase(), 'garmin.private-extension-manager-remote-helper');
+        assert.strictEqual(extension?.id?.toLowerCase(), EXTENSION_ID);
         assert.strictEqual(extension?.extensionKind, vscode.ExtensionKind.UI);
-        assert.strictEqual(extension?.packageJSON.name, 'private-extension-manager-remote-helper');
-        assert.strictEqual(extension?.packageJSON.publisher, 'garmin');
+        assert.strictEqual(extension?.packageJSON.name, EXTENSION_NAME);
+        assert.strictEqual(extension?.packageJSON.publisher, PUBLISHER_ID);
         assert.strictEqual(typeof extension?.packageJSON.version, 'string');
     });
 
